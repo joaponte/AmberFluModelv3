c1d45eb5770389b3fdf131e067f21d27e47641f3
joaponte <josua.aponte@gmail.com>
2020-05-07 18:44:47 -0400

    My working version

--- Simulation/AmberFluModel.py
+++ Simulation/AmberFluModel.py
@@ -1,10 +1,10 @@
 
 from cc3d import CompuCellSetup
         
-
 from AmberFluModelSteppables import AmberFluModelSteppable
-
 CompuCellSetup.register_steppable(steppable=AmberFluModelSteppable(frequency=1))
 
+from AmberFluModelSteppables import CellularModelSteppable
+CompuCellSetup.register_steppable(steppable=CellularModelSteppable(frequency=1))
 
 CompuCellSetup.run()
